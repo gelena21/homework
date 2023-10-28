58706ff510eec6a2772419fc5cf0716fddbecc74
Gelena Bezusco <helenatcitok@gmail.com>
2023-10-28 11:22:27 +0100

    add 2 functions

---
+++ src/masks.py
@@ -0,0 +1,10 @@
+def mask_card_number(card_number: str) -> str:
+    """Функция для маскировки номера карты пользователя"""
+    masked_number = f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
+    return masked_number
+
+
+def mask_account_number(account_number: str) -> str:
+    """Функция для маскировки номера счета пользователя"""
+    masked_number = f"**{account_number[-4:]}"
+    return masked_number
