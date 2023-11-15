548ae4ebde201b332c88d79d901537843a8b023b
Gelena Bezusco <helenatcitok@gmail.com>
2023-11-15 21:08:23 +0000

    add loggers

--- src/masks.py
+++ src/masks.py
@@ -1,3 +1,16 @@
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
+file_handler = logging.FileHandler("masks.log", mode="w", encoding="utf-8")
+file_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+
+
 def mask_card_number(card_number: str) -> str:
     """Функция для маскировки номера карты пользователя"""
     masked_number = f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,14 @@
 import json
+import logging
 import os
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+file_handler = logging.FileHandler("utils.log")
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
 
 
 def load_transactions(file_path):
